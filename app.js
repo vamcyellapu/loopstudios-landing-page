69b648370ef9564fbc26c2960b566265e1bb3e25
vamcyellapu <vamsiyellapu@yahoo.com>
2022-05-18 16:09:42 +0530

    grid redo

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-'use strict';
+// 'use strict';
 
-const creation = document.querySelectorAll('.creation');
-const images = document.querySelectorAll('.image');
-const imagesBig = [
-  './images/desktop/image-deep-earth.jpg',
-  './images/desktop/image-night-arcade.jpg',
-  './images/desktop/image-soccer-team.jpg',
-  './images/desktop/image-grid.jpg',
-  './images/desktop/image-from-above.jpg',
-  './images/desktop/image-pocket-borealis.jpg',
-  './images/desktop/image-curiosity.jpg',
-  './images/desktop/image-fisheye.jpg',
-];
-const imagesSmall = [
-  './images/mobile/image-deep-earth.jpg',
-  './images/mobile/image-night-arcade.jpg',
-  './images/mobile/image-soccer-team.jpg',
-  './images/mobile/image-grid.jpg',
-  './images/mobile/image-from-above.jpg',
-  './images/mobile/image-pocket-borealis.jpg',
-  './images/mobile/image-curiosity.jpg',
-  './images/mobile/image-fisheye.jpg',
-];
+// const creation = document.querySelectorAll('.creation');
+// const images = document.querySelectorAll('.image');
+// const imagesBig = [
+//   './images/desktop/image-deep-earth.jpg',
+//   './images/desktop/image-night-arcade.jpg',
+//   './images/desktop/image-soccer-team.jpg',
+//   './images/desktop/image-grid.jpg',
+//   './images/desktop/image-from-above.jpg',
+//   './images/desktop/image-pocket-borealis.jpg',
+//   './images/desktop/image-curiosity.jpg',
+//   './images/desktop/image-fisheye.jpg',
+// ];
+// const imagesSmall = [
+//   './images/mobile/image-deep-earth.jpg',
+//   './images/mobile/image-night-arcade.jpg',
+//   './images/mobile/image-soccer-team.jpg',
+//   './images/mobile/image-grid.jpg',
+//   './images/mobile/image-from-above.jpg',
+//   './images/mobile/image-pocket-borealis.jpg',
+//   './images/mobile/image-curiosity.jpg',
+//   './images/mobile/image-fisheye.jpg',
+// ];
 
-window.addEventListener('resize', function () {
-  const width = this.window.innerWidth;
-  if (width >= 768) {
-    images.forEach((image, i) => {
-      image.src = imagesBig[i];
-    });
-  } else {
-    images.forEach((image, i) => {
-      image.src = imagesSmall[i];
-    });
-  }
-});
+// window.addEventListener('resize', function () {
+//   const width = this.window.innerWidth;
+//   if (width >= 768) {
+//     images.forEach((image, i) => {
+//       image.src = imagesBig[i];
+//     });
+//   } else {
+//     images.forEach((image, i) => {
+//       image.src = imagesSmall[i];
+//     });
+//   }
+// });
