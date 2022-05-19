b8a180e49f130a7766d474715dc7969800d2993d
vamcyellapu <vamsiyellapu@yahoo.com>
2022-05-19 11:21:22 +0530

    build the creations section

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-// 'use strict';
+'use strict';
+
+const creationsContainer = document.querySelector('.creations-container');
+const creationsHeader = document.querySelector('.creations-header');
+const btn = document.querySelector('.btn');
+console.log(creationsContainer);
+const resize = function () {
+  let width = window.window.innerWidth;
+  console.log(width);
+  if (width < 768) {
+    creationsContainer.append(btn);
+  } else {
+    creationsHeader.append(btn);
+  }
+};
+
+resize();
+window.addEventListener('resize', resize);
 
 // const creation = document.querySelectorAll('.creation');
 // const images = document.querySelectorAll('.image');
