cb2fbbd79105e97fb6c518cc15409056fb7fb4ce
vamcyellapu <vamsiyellapu@yahoo.com>
2022-05-19 20:18:34 +0530

    build the header section

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 'use strict';
 
+const menuIcon = document.querySelector('.menu-icon');
+const navList = document.querySelector('.navigation__list');
+const icon = document.querySelector('.icon-main');
+const heroHeading = document.querySelector('.hero__heading');
+console.log(icon);
+menuIcon.addEventListener('click', function (e) {
+  navList.classList.toggle('display');
+  const iconIsTrue = icon.href.baseVal === '/images/sprite.svg#icon-hamburger';
+
+  if (iconIsTrue) {
+    icon.href.baseVal = '/images/sprite.svg#icon-close';
+    heroHeading.classList.add('display');
+  } else {
+    icon.href.baseVal = '/images/sprite.svg#icon-hamburger';
+    heroHeading.classList.remove('display');
+  }
+});
+
 // const creationsContainer = document.querySelector('.creations-container');
 // const creationsHeader = document.querySelector('.creations__header');
 // const btn = document.querySelector('.btn');
